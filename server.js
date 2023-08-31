ce77082b4dc0d9af1b414cf8497de9d9fb0747cf
evmdaredevil <69949583+evmdaredevil@users.noreply.github.com>
2023-08-31 17:47:27 -0600

    Primer avance

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const { Client } = require('pg');
+const app = express();
+const port = 3000;
+
+const dbConfig = {
+  user: 'postgres',
+  host: 'localhost',
+  database: 'cenapred_db',
+  password: 'cenapred',
+  port: 5433,
+};
+
+const client = new Client(dbConfig);
+client.connect();
+
+app.use(express.static('public')); // Serve static files from "public" folder
+
+app.get('/getTables', async (req, res) => {
+  try {
+    const query = `
+      SELECT table_name
+      FROM information_schema.tables
+      WHERE table_schema = 'public'
+      AND table_type = 'BASE TABLE'
+    `;
+
+    const result = await client.query(query);
+    const tables = result.rows.map(row => row.table_name);
+    res.json(tables);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error fetching tables');
+  }
+});
+
+app.get('/getTableData', async (req, res) => {
+  const tableName = req.query.tableName;
+
+  try {
+    const query = `SELECT * FROM estados`;
+    //const query = `SELECT * FROM ${tableName}`;
+    const result = await client.query(query);
+    const data = result.rows;
+    res.json(data);
+  } catch (err) {
+    console.error('Database Query Error:', err); // Log the error
+    res.status(500).send('Error fetching table data');
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
