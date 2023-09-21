4ca73b3cad11d22168c81a258f2aa9cbc747174a
evmdaredevil <69949583+evmdaredevil@users.noreply.github.com>
2023-09-21 14:14:47 -0600

    Mejoras al estilo del Formulario

--- PortalCERT Mk2/script.js
+++ PortalCERT Mk2/script.js
@@ -1,32 +1,38 @@
+
 const overlay = document.getElementById('overlay');
-        const cerrarButton = document.getElementById('cerrarButton');
+const cerrarButton = document.getElementById('cerrarButton');
 
-        cerrarButton.addEventListener('click', function () {
-            overlay.style.display = 'none';
-        });
+cerrarButton.addEventListener('click', function () {
+    overlay.style.display = 'none';
+});
 
-        // Immediately show the overlay when the page loads
-        window.addEventListener('load', function () {
-            overlay.style.display = 'block';
-        });
+// Immediately show the overlay when the page loads
+window.addEventListener('load', function () {
+    overlay.style.display = 'block';
+});
 
-        const mapa = L.map('mapa').setView([23.6345, -102.5528], 6); 
-        L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-            attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-        }).addTo(mapa);
-        let lastMarker = null; 
+const mapa = L.map('mapa').setView([23.6345, -102.5528], 6);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(mapa);
 
-        mapa.on('click', function (e) {
-            const latitud = e.latlng.lat;
-            const longitud = e.latlng.lng;
+const search = new GeoSearch.GeoSearchControl({
+    provider: new GeoSearch.OpenStreetMapProvider(),
+    });
+mapa.addControl(search);
+let lastMarker = null;
 
-            document.getElementById('Latitud').value = latitud;
-            document.getElementById('Longitud').value = longitud;
-            // Remove the previous marker, if any
-            if (lastMarker) {
-                mapa.removeLayer(lastMarker);
-            }
+mapa.on('click', function (e) {
+    const latitud = e.latlng.lat;
+    const longitud = e.latlng.lng;
 
-            // Add a new marker at the clicked location
-            lastMarker = L.marker([latitud, longitud]).addTo(mapa);
-        });
+    document.getElementById('Latitud').value = latitud;
+    document.getElementById('Longitud').value = longitud;
+    // Remove the previous marker, if any
+    if (lastMarker) {
+        mapa.removeLayer(lastMarker);
+    }
+
+    // Add a new marker at the clicked location
+    lastMarker = L.marker([latitud, longitud]).addTo(mapa);
+});
