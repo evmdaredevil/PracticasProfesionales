7af06e3254e83674d16114549f915dd4430e1b80
Edgar <evm_daredevil@hotmail.com>
2023-12-04 09:55:43 -0600

    Update index.php

--- Guide/LocalSHP/index.php
+++ Guide/LocalSHP/index.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shapefile Visualizer</title>
-    <!-- Leaflet CSS -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
     <style>
         #map {
-            height: 500px;
+            height: 1000px;
         }
     </style>
 </head>
@@ -16,12 +15,9 @@
 
 <div id="map"></div>
 
-<!-- Leaflet JS -->
 <script src="leaflet/leaflet.js"></script>
-<!-- Proj4Leaflet JS -->
 <script src="proj4.js"></script>
 <script src="proj4leaflet.js"></script>
-<!-- Leaflet Shapefile plugin -->
 <script src="leaflet/leaflet.shpfile.js"></script>
 <script src="leaflet/shp.js"></script>
 
@@ -32,22 +28,29 @@
         zoom: 2
     });
 
-    // Add OpenStreetMap base layer
+    //Capa Base (OSM)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap contributors'
     }).addTo(map);
 
-    // Add shapefile layer
-   //var shapefileLayer = new L.Shapefile('https://www.inegi.org.mx/contenidos/productos/prod_serv/contenidos/espanol/bvinegi/productos/geografia/imagen_cartografica/1_50_000/889463861379_s.zip', {
+    //Para no tener problemas con CORS, la capa debe estar en el mismo directorio que index.php
    var shapefileLayer = new L.Shapefile('http://localhost/LocalSHP/agebs.zip', {
-
      onEachFeature: function (feature, layer) {
             console.log('Shapefile Feature Properties:', feature.properties);
-            // You can customize popup content here
+            // Muestra el atributo CVE_AGEB de la capa
             layer.bindPopup("Attribute: " + feature.properties.CVE_AGEB);
         }
     });
     shapefileLayer.addTo(map);
+
+    var secondshapefileLayer = new L.Shapefile('http://localhost/LocalSHP/zonas_de_cultivo.zip', {
+     onEachFeature: function (feature, layer) {
+            console.log('Shapefile Feature Properties for Second Layer:', feature.properties);
+            // Muestra el atributo CVE_AGEB de la capa
+            layer.bindPopup("Attribute: " + feature.properties.CVE_AGEB);
+        }
+    });
+    secondshapefileLayer.addTo(map);
 </script>
 
 </body>
